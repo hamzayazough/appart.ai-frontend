4c1253ed130c9130551de71c26687909ef29c162
Antoine-Lafrance <antoine.lafrance@polymtl.ca>
2024-08-26 19:19:47 -0400

    added landing page header

--- appart-ai-frontend/src/app/app.module.ts
+++ appart-ai-frontend/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing/landing-page/landing-page.component';
+import { LandingHeaderComponent } from './pages/landing/landing-header/landing-header.component';
+import { GradientButtonComponent } from './shared/gradient-button/gradient-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LandingHeaderComponent,
+    GradientButtonComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ appart-ai-frontend/src/app/shared/gradient-button/gradient-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gradient-button',
+  templateUrl: './gradient-button.component.html',
+  styleUrl: './gradient-button.component.scss'
+})
+export class GradientButtonComponent {
+
+}
