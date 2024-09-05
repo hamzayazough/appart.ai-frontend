2195f90ca6c151923c823d23045c57166fad37f6
CarlEric-Lafleur <carl-eric.lafleur@polymtl.ca>
2024-09-05 16:18:16 -0400

    add account page

--- appart-ai-frontend/src/app/app.module.ts
+++ appart-ai-frontend/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing/landing-page/landing-page.component';
 import { LandingHeaderComponent } from './pages/landing/landing-header/landing-header.component';
 import { SharedModule } from './shared/shared.module';
+import { AccountPageComponent } from './pages/account/account-page/account-page.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent, LandingHeaderComponent],
+  declarations: [AppComponent, LandingPageComponent, LandingHeaderComponent, AccountPageComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule, HammerModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
